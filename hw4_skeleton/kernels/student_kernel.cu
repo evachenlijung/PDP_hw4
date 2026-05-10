25b6bb9aed73e67e1557032aeb1f76c507b16064
Eva Chen <eva111708@hgmail.com>
2026-05-10 22:03:21 +0800

    to hint 6

--- hw4_skeleton/kernels/student_kernel.cu
+++ hw4_skeleton/kernels/student_kernel.cu
@@ -71,37 +71,73 @@ __global__ void StudentKernel(int M, int N, int K, float alpha,
     float regA[TM]; // read a column from sA
     float regB[TN]; // read a row from sB
 
-    int a_stride = BLOCK_THREADS / BK;
-    int b_stride = BLOCK_THREADS / BN;
+    int a_stride = BLOCK_THREADS / (BK/4);
+    int b_stride = BLOCK_THREADS / (BN/4);
 
-    // 搬 A, B 的起始位置
-    int a_load_row = threadIdx.x / BK;
-    int a_load_col = threadIdx.x % BK;
-    int b_load_row = threadIdx.x / BN;
-    int b_load_col = threadIdx.x % BN;
+    // // 搬 A, B 的起始位置
+    // int a_load_row = threadIdx.x / BK;
+    // int a_load_col = threadIdx.x % BK;
+    // int b_load_row = threadIdx.x / BN;
+    // int b_load_col = threadIdx.x % BN;
+
+    // move A, B, 4 floats per step
+    int a_load_row = threadIdx.x / (BK/4);
+    int a_load_col = threadIdx.x % (BK/4);
+    int b_load_row = threadIdx.x / (BN/4);
+    int b_load_col = threadIdx.x % (BN/4);
 
     int a_row_base = block_row + a_load_row;
-    int b_col_base = block_col + b_load_col;
+    // int b_col_base = block_col + b_load_col;
 
     // 沿 K 方向每次處理 BK 寬的寬帶
     for(int t=0; t < CEIL_DIV(K, BK); t++){
-        // 搬 A tile 進 shared memory
+        // // 搬 A tile 進 shared memory
+        // for(int i=0; i<BM; i+=a_stride){
+        //     int global_row = a_row_base + i;
+        //     int global_col = BK * t + a_load_col;
+        //     sA[a_load_col][a_load_row + i] = 
+        //         (global_row < M && global_col < K) 
+        //         ? A[K * global_row + global_col] : 0.0f;
+        // }
+
+        int a_global_col = BK * t + a_load_col * 4;
         for(int i=0; i<BM; i+=a_stride){
-            int global_row = a_row_base + i;
-            int global_col = BK * t + a_load_col;
-            sA[a_load_col][a_load_row + i] = 
-                (global_row < M && global_col < K) 
-                ? A[K * global_row + global_col] : 0.0f;
+            int a_global_row = a_row_base + i;
+            if(a_global_row < M && a_global_col + 3 < K){
+                float4 tmp = *reinterpret_cast<float4*>(&A[a_global_row * K + a_global_col]);
+                sA[a_load_col * 4 + 0][a_load_row + i] = tmp.x;
+                sA[a_load_col * 4 + 1][a_load_row + i] = tmp.y;
+                sA[a_load_col * 4 + 2][a_load_row + i] = tmp.z;
+                sA[a_load_col * 4 + 3][a_load_row + i] = tmp.w;
+            }else{
+                sA[a_load_col * 4 + 0][a_load_row + i] = 0.0f;
+                sA[a_load_col * 4 + 1][a_load_row + i] = 0.0f;
+                sA[a_load_col * 4 + 2][a_load_row + i] = 0.0f;
+                sA[a_load_col * 4 + 3][a_load_row + i] = 0.0f;
+            }
         }
 
-        // 搬 B tile 進 shared memory
+        // // 搬 B tile 進 shared memory
+        // int b_row_base = BK * t + b_load_row;
+        // for(int i=0; i<BK; i+=b_stride){
+        //     int global_row = b_row_base + i;
+        //     // int global_col = col_base;
+        //     sB[b_load_row + i][b_load_col] = 
+        //         (global_row < K && b_col_base < N) 
+        //         ? B[N * global_row + b_col_base] : 0.0f;
+        // }
+
         int b_row_base = BK * t + b_load_row;
+        int b_global_col = block_col + b_load_col * 4;
         for(int i=0; i<BK; i+=b_stride){
-            int global_row = b_row_base + i;
-            // int global_col = col_base;
-            sB[b_load_row + i][b_load_col] = 
-                (global_row < K && b_col_base < N) 
-                ? B[N * global_row + b_col_base] : 0.0f;
+            int b_global_row = b_row_base + i;
+            if(b_global_row < K && b_global_col + 3 < N){
+                *reinterpret_cast<float4*>(&sB[b_load_row + i][b_load_col * 4]) 
+                    = *reinterpret_cast<float4*>(&B[b_global_row * N + b_global_col]);
+            }else{
+                *reinterpret_cast<float4*>(&sB[b_load_row + i][b_load_col * 4]) 
+                    = make_float4(0,0,0,0);
+            }
         }
 
         __syncthreads();
